a3c1fc0db18a1105bb9a90615bf1df0069b2c4b6
pbkp2611 <56658320+pbkp2611@users.noreply.github.com>
2023-04-07 01:58:06 +0530

    Add files via upload

---
+++ src/utility/CreateScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function CreateScreen(): JSX.Element {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Create screen</Text>
+    </View>
+  );
+}
+
+export default CreateScreen
