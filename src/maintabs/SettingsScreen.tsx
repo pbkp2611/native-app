4b2edb2e954cb4401f8b228f1cd8d55f45cbd5f2
pbkp2611 <56658320+pbkp2611@users.noreply.github.com>
2023-05-18 19:55:40 +0530

    Add files via upload

--- src/maintabs/SettingsScreen.tsx
+++ src/maintabs/SettingsScreen.tsx
@@ -1,12 +1,14 @@
 import React, { useCallback, useState } from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
-import { Button, Image } from '@rneui/base';
+import { SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { Image } from '@rneui/base';
 import { useDispatch, useSelector } from 'react-redux';
 import { logOut, selectCurrentToken } from '../auth/authSlice';
 import jwtDecode from 'jwt-decode';
 import Share from 'react-native-share'
 import {Asset, ImagePickerResponse, launchCamera, launchImageLibrary, MediaType } from 'react-native-image-picker';
-import { Dialog } from '@rneui/themed';
+import { Dialog, Divider, ListItem } from '@rneui/themed';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faAngleRight, faCog, faLanguage, faLock, faQuestionCircle, faShareAlt, faSignOut, faUser } from '@fortawesome/free-solid-svg-icons';
 
 export default function SettingsScreen(): JSX.Element {
 
@@ -76,44 +78,102 @@ export default function SettingsScreen(): JSX.Element {
     setUri(uri)
   }, []);
 
-  // const uri = pickerResponse?.assets && pickerResponse.assets[0].uri;
-
-
-
-
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Image
+    <ScrollView>
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <View style={{flex:1, flexDirection:'row', margin:8}}>
+        <Image
             source={{ uri:uri?uri:'https://image.shutterstock.com/image-photo/image-260nw-702310516.jpg' }}
             // containerStyle={styles.item}
-            style={{ height: 160, width: 160, borderRadius:80}}
+            style={{ height: 80, width: 80, borderRadius:40}}
             PlaceholderContent={<Text>Loading...</Text>}
-      />
-      <Button
-        title="Select image"
-        onPress={() => setVisible(true)}
-      />
+        />
+        <View>
+          <Text style={{marginTop:8, marginStart:12, fontSize:22}}>{username}</Text>
+          <Text style={{marginStart:12, fontSize:18}}>@suzume.mota123</Text>
+        </View>
+      </View>
       <ImagePickerModal
         isVisible={visible}
         onClose={() => setVisible(false)}
         onImageLibraryPress={onImageLibraryPress}
         onCameraPress={onCameraPress}
       />
-      <Text>-</Text>
-      <Text>User Name - {username}</Text>
-      <Text>Role - {roles}</Text>
-      <Text>-</Text>
-      <Button onPress={async () => {
+
+      <View style={{margin:4, flexDirection:'row'}}>
+        <View style={{flex:1, alignItems:'center', marginRight:4, marginLeft:4, paddingBottom:4, backgroundColor:'white'}}>
+          <Text style={{fontSize:22}}>Followers</Text>
+          <Text>12K</Text>
+        </View>
+        <View style={{flex:1, alignItems:'center', marginRight:4, marginLeft:4, paddingBottom:4, backgroundColor:'white'}}>
+          <Text style={{fontSize:22}}>Followings</Text>
+          <Text>2K</Text>
+        </View>
+      </View>
+
+      <View style={{margin:4}}>
+        <ListItem>
+          <FontAwesomeIcon icon={faUser} />
+          <ListItem.Content>
+            <ListItem.Title>Account</ListItem.Title>
+          </ListItem.Content>
+          <FontAwesomeIcon icon={faAngleRight} />
+        </ListItem>
+        <Divider width={1} color='#e6e3e3'/>
+        <ListItem>
+          <FontAwesomeIcon icon={faLock} />
+          <ListItem.Content>
+            <ListItem.Title>Privacy</ListItem.Title>
+          </ListItem.Content>
+          <FontAwesomeIcon icon={faAngleRight} />
+        </ListItem>
+        <Divider width={1} color='#e6e3e3'/>
+        <ListItem>
+          <FontAwesomeIcon icon={faLanguage} />
+          <ListItem.Content>
+            <ListItem.Title>Language</ListItem.Title>
+          </ListItem.Content>
+          <FontAwesomeIcon icon={faAngleRight} />
+        </ListItem>
+        <Divider width={1} color='#e6e3e3'/>
+        <ListItem>
+          <FontAwesomeIcon icon={faCog} />
+          <ListItem.Content>
+            <ListItem.Title>Settings</ListItem.Title>
+          </ListItem.Content>
+          <FontAwesomeIcon icon={faAngleRight} />
+        </ListItem>
+      </View>
+
+      <View style={{marginTop:4,marginLeft:4, marginRight:4, marginBottom:8}}>
+        <ListItem>
+          <FontAwesomeIcon icon={faQuestionCircle} />
+          <ListItem.Content>
+            <ListItem.Title>Help</ListItem.Title>
+          </ListItem.Content>
+          <FontAwesomeIcon icon={faAngleRight} />
+        </ListItem>
+        <Divider width={1} color='#e6e3e3'/>
+        <ListItem onPress={async () => {
           await shareApp();
-        }} title="Share the App"
-      />
-      <Text>-</Text>
-      <Button
-        title="Logout"
-        // style={{ marginTop: 20 }}
-        onPress={() => logOutUser()}
-      />
+        }}>
+          <FontAwesomeIcon icon={faShareAlt} />
+          <ListItem.Content>
+            <ListItem.Title>Share</ListItem.Title>
+          </ListItem.Content>
+          <FontAwesomeIcon icon={faAngleRight} />
+        </ListItem>
+        <Divider width={1} color='#e6e3e3'/>
+        <ListItem onPress={() => logOutUser()}>
+          <FontAwesomeIcon icon={faSignOut} />
+          <ListItem.Content>
+            <ListItem.Title>Logout</ListItem.Title>
+          </ListItem.Content>
+          <FontAwesomeIcon icon={faAngleRight} />
+        </ListItem>
+      </View>
     </View>
+    </ScrollView>
   );
 }
 
